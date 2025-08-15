c21ee7929564016c82a3de8a4bb3faefafa1595a
Fyandono <riva.alifyandono@gmail.com>
2025-08-15 14:13:48 +0700

    Update whisps page

--- lib/features/whisps/presentation/page/whisp.dart
+++ lib/features/whisps/presentation/page/whisp.dart
@@ -35,158 +35,161 @@ class WhispsPage extends StatelessWidget {
       textController.clear();
     }
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: AppColors.white,
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color: AppColors.greyBorder, width: 2),
+    return PopScope(
+      canPop: false,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColors.white,
+          body: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: AppColors.greyBorder, width: 2),
+                  ),
                 ),
-              ),
-              child: BlocBuilder<ProfileBloc, ProfileState>(
-                builder: (context, state) {
-                  Random random = Random();
-                  Widget username = MyShimmer(
-                    height: 14,
-                    width: random.nextInt(96).toDouble(),
-                  );
-                  Widget avatar = CircleAvatar(
-                    radius: 16,
-                    backgroundColor: AppColors.greyBorder,
-                  );
-                  Widget follower = Row(
-                    children: [
-                      Icon(Icons.people, size: 12),
-                      SizedBox(width: 4),
-                      MyShimmer(height: 14, width: 24),
-                    ],
-                  );
-                  Widget relate = Row(
-                    children: [
-                      MyText(text: "relate", fontSize: 14),
-                      SizedBox(width: 4),
-                      MyShimmer(height: 14, width: 24),
-                    ],
-                  );
-                  if (state is ProfileLoaded) {
-                    avatar = PixelAvatarPreview(
-                      pixels: state.pixelAvatar,
-                      size: 32,
+                child: BlocBuilder<ProfileBloc, ProfileState>(
+                  builder: (context, state) {
+                    Random random = Random();
+                    Widget username = MyShimmer(
+                      height: 14,
+                      width: random.nextInt(96).toDouble(),
                     );
-                    username = MyText(
-                      text: "@${state.username}",
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
+                    Widget avatar = CircleAvatar(
+                      radius: 16,
+                      backgroundColor: AppColors.greyBorder,
                     );
-
-                    follower = Row(
+                    Widget follower = Row(
                       children: [
-                        Icon(Icons.people, size: 14),
+                        Icon(Icons.people, size: 12),
                         SizedBox(width: 4),
-                        MyText(
-                          text: "20K",
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                        ),
+                        MyShimmer(height: 14, width: 24),
                       ],
                     );
-                    relate = Row(
+                    Widget relate = Row(
                       children: [
-                        MyText(text: "relate", fontSize: 12),
+                        MyText(text: "Reaction", fontSize: 14),
                         SizedBox(width: 4),
-                        MyText(
-                          text: "100K",
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                        ),
+                        MyShimmer(height: 14, width: 24),
                       ],
                     );
-                  }
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 24,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        avatar,
-                        SizedBox(width: 8),
-                        username,
-                        Spacer(),
-                        SizedBox(width: 8),
-                        follower,
-                        SizedBox(width: 8),
-                        relate,
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            Expanded(
-              child: BlocBuilder<WhispsBloc, WhispsState>(
-                builder: (context, state) {
-                  // initial or loading
-                  if (state is WhispsInitial || state is WhispsLoading) {
-                    return const MyWhispsLoading();
-
-                    // loaded
-                  } else if (state is WhispsLoaded) {
-                    controller.addListener(() {
-                      if (controller.position.maxScrollExtent ==
-                              controller.offset &&
-                          state.hasMore) {
-                        bloc.add(LoadMoreWhisps());
-                      }
-                    });
-
-                    // list loaded
-                    if (state.listObject.isNotEmpty) {
-                      return Column(
+                    if (state is ProfileLoaded) {
+                      avatar = PixelAvatarPreview(
+                        pixels: state.pixelAvatar,
+                        size: 32,
+                      );
+                      username = MyText(
+                        text: "@${state.username}",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      );
+      
+                      follower = Row(
                         children: [
-                          Expanded(
-                            child: MyWhispsLoaded(
-                              controller: controller,
-                              listObject: state.listObject,
-                              hasMore: state.hasMore,
-                            ),
+                          Icon(Icons.people_outline, size: 14),
+                          SizedBox(width: 4),
+                          MyText(
+                            text: "20K",
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
                           ),
-                          BlocProvider(
-                            create: (context) => WhispLabelCubit(),
-                            child: WhispField(
-                              controller: textController,
-                              method: method,
-                            ),
+                        ],
+                      );
+                      relate = Row(
+                        children: [
+                          MyText(text: "Reaction", fontSize: 12),
+                          SizedBox(width: 4),
+                          MyText(
+                            text: "100K",
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
                           ),
                         ],
                       );
                     }
-                    // list empty
-                    else {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 24,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          avatar,
+                          SizedBox(width: 8),
+                          username,
+                          Spacer(),
+                          SizedBox(width: 8),
+                          follower,
+                          SizedBox(width: 8),
+                          relate,
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: BlocBuilder<WhispsBloc, WhispsState>(
+                  builder: (context, state) {
+                    // initial or loading
+                    if (state is WhispsInitial || state is WhispsLoading) {
+                      return const MyWhispsLoading();
+      
+                      // loaded
+                    } else if (state is WhispsLoaded) {
+                      controller.addListener(() {
+                        if (controller.position.maxScrollExtent ==
+                                controller.offset &&
+                            state.hasMore) {
+                          bloc.add(LoadMoreWhisps());
+                        }
+                      });
+      
+                      // list loaded
+                      if (state.listObject.isNotEmpty) {
+                        return Column(
+                          children: [
+                            Expanded(
+                              child: MyWhispsLoaded(
+                                controller: controller,
+                                listObject: state.listObject,
+                                hasMore: state.hasMore,
+                              ),
+                            ),
+                            BlocProvider(
+                              create: (context) => WhispLabelCubit(),
+                              child: WhispField(
+                                controller: textController,
+                                method: method,
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                      // list empty
+                      else {
+                        return const MyEmptyList(
+                          icon: Icons.chat_bubble_outline,
+                          title: "It's a little quiet here...",
+                          subtitle:
+                              "No whisps yet. Be the first to say something!",
+                        );
+                      }
+      
+                      // else or error
+                    } else {
                       return const MyEmptyList(
-                        icon: Icons.chat_bubble_outline,
-                        title: "It's a little quiet here...",
-                        subtitle:
-                            "No whisps yet. Be the first to say something!",
+                        icon: Icons.wifi_off,
+                        title: 'Whoops! Something went wrong 😕',
+                        subtitle: 'Please check your connection and try again.',
                       );
                     }
-
-                    // else or error
-                  } else {
-                    return const MyEmptyList(
-                      icon: Icons.wifi_off,
-                      title: 'Whoops! Something went wrong 😕',
-                      subtitle: 'Please check your connection and try again.',
-                    );
-                  }
-                },
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
